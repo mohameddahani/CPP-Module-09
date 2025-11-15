decc10ddd68d5435fe813fe3681c5d355384b5cc
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-11-15 21:11:06 +0100

    recode the algo :(

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,11 +6,73 @@
 /*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 08:24:34 by mdahani           #+#    #+#             */
-/*   Updated: 2025/11/13 11:33:59 by mdahani          ###   ########.fr       */
+/*   Updated: 2025/11/15 21:10:27 by mdahani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+
+// * Calculation the number of Comparisons
+static unsigned long long comparisons = 0;
+
+bool comparison(long a, long b){
+    comparisons++;
+    return a > b;
+}
+
+
+// ! DON'T FORGET FLAGS IN MAKEFILE
+
+
+
+static void divisionIntoPairsAndSorting(std::vector<long>&vector, int sizeOfPairs){
+    // * vector
+    // 10 9 8 7 6 5 4 3 2 1 0
+    // [9 10] [7 8] [5 6] [3 4] [1 2] 0
+    // [a1 b1] [a2 b2] [a3 b3] [a4 b4] [a5 b5] a6
+
+    // ! 0-1 | 2-3 (index comparison in every recusion)
+    // ! 1-3 | 5-7 (index comparison in every recusion)
+    // ! 3-7 | 11-15 (index comparison in every recusion)
+
+    // [(7 8) (9 10)] [(3 4) (5 6)] 1 2 0
+    // [(3 4 5 6) (7 8 9 10)] 1 2 0
+    // 3 4 5 6 7 8 9 10             1 2 0
+
+
+    
+                        // 0 + 1 < 11
+
+    // ! Need to swap raaaaaange
+    for (size_t i = 0; i + sizeOfPairs / 2 < vector.size(); i+= sizeOfPairs){
+        size_t a = i;
+        size_t b = sizeOfPairs / 2;
+
+        if (comparison(vector[a], vector[b])){
+            std::swap(vector[a], vector[b]);
+        }
+        std::cout << i + sizeOfPairs / 2 << std::endl;
+
+
+        std::vector<long>::iterator it = vector.begin();
+        std::cout << "=============================\n";
+        for (; it != vector.end(); ++it){
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "=============================\n";
+        
+    }
+    
+    
+    // * condition of stop recusion that's mean when we division into pairs and we has only 1 pairs then we need to stop
+    if (vector.size() / sizeOfPairs == 1){
+        return;
+    }
+
+    // * recall the function
+    divisionIntoPairsAndSorting(vector, sizeOfPairs * 2);
+}
 
 static double getTimeByUs(){
     // ? timespec this is like a `struct timeval' but has nanoseconds not microseconds
@@ -105,127 +167,139 @@ void mergeInsertionSort(int ac, char **av){
 
     std::cout << std::endl;
 
-    // * check if we have a even or odd vector and deque if is odd we need to store the last element in variable and the remove it to be contaier even
-    bool isEven = true;
-    long lastElement = 0;
-    if (vector.size() % 2 != 0){
-        lastElement = vector.back();
-        vector.pop_back();
-        deque.pop_back();
+    // todo: Step 1: the division into the pairs & sorting
+    divisionIntoPairsAndSorting(vector, 2);
 
-        isEven = false;
-    }
-
-    // ! Sort the Vector
-    // * get time by microseconds befor sorting 
-    double startTime = getTimeByUs();
-
-
-    // * Main chain and Second chain of vector
-    std::vector<long>vectorMainChain;
-    std::vector<long>vectorSecondChain;
-
-    // * Dividing into pairs and Sort the pairs in vector
-    for (size_t i = 0; i < vector.size(); i+= 2){
-        // * vector
-        if (vector[i] > vector[i + 1]){
-            std::swap(vector[i], vector[i + 1]);
-        }
+    // * print the vector after every step
+    std::vector<long>::iterator it = vector.begin();
+    for (; it != vector.end(); ++it){
+        std::cout << *it << " ";
     }
     
-    // * Forming two chains in vector
-    for (size_t i = 0; i < vector.size(); i+= 2){
-        // * vector
-        vectorMainChain.push_back(vector[i + 1]);
-        vectorSecondChain.push_back(vector[i]);
-    }
-
-    // * check if we has a odd vector (push the last element that we remove it in the begin in second chain of vector)
-    if (!isEven){
-        // * vector
-        vectorSecondChain.push_back(lastElement);
-    }
+    std::cout << *it << std::endl;
     
 
-    // * Sort the main chain of vector
-    std::sort(vectorMainChain.begin(), vectorMainChain.end());
+    // // * check if we have a even or odd vector and deque if is odd we need to store the last element in variable and the remove it to be contaier even
+    // bool isEven = true;
+    // long lastElement = 0;
+    // if (vector.size() % 2 != 0){
+    //     lastElement = vector.back();
+    //     vector.pop_back();
+    //     deque.pop_back();
 
-    // * Merge chains of vector (Add the small numbers to main chain: every single one we need to add him to correct place)
-    std::vector<long>::iterator itVectorInsert;
-    for (size_t i = 0; i < vectorSecondChain.size(); i++){
-        // * vector
-        itVectorInsert = std::lower_bound(vectorMainChain.begin(), vectorMainChain.end(), vectorSecondChain[i]);
-        vectorMainChain.insert(itVectorInsert, vectorSecondChain[i]);
-    }
+    //     isEven = false;
+    // }
+
+    // // ! Sort the Vector
+    // // * get time by microseconds befor sorting 
+    // double startTime = getTimeByUs();
+
+
+    // // * Main chain and Second chain of vector
+    // std::vector<long>vectorMainChain;
+    // std::vector<long>vectorSecondChain;
+
+    // // * Dividing into pairs and Sort the pairs in vector
+    // for (size_t i = 0; i < vector.size(); i+= 2){
+    //     // * vector
+    //     if (vector[i] > vector[i + 1]){
+    //         std::swap(vector[i], vector[i + 1]);
+    //     }
+    // }
     
-    // * get time by microseconds after sorting 
-    double endTime = getTimeByUs();
+    // // * Forming two chains in vector
+    // for (size_t i = 0; i < vector.size(); i+= 2){
+    //     // * vector
+    //     vectorMainChain.push_back(vector[i + 1]);
+    //     vectorSecondChain.push_back(vector[i]);
+    // }
 
-    // * print all element after sorting
-    std::vector<long>::iterator vectorItAfterSort = vectorMainChain.begin();
-    
-    std::cout << "After: ";
-    
-    for(; vectorItAfterSort != vectorMainChain.end(); ++vectorItAfterSort){
-        std::cout << *vectorItAfterSort << " ";
-    }
-
-    std::cout << std::endl;
-
-    // * print time of excution by microseconds
-    std::cout << std::fixed << std::setprecision(5)
-        << "Time to process a range of "
-        << (isEven ? vector.size() : vector.size() + 1) << " elements with std::[vector] : "
-        << endTime - startTime << "µs" << std::endl;
-
-
-    // ! Sort the Deque
-    // * get time by microseconds befor sorting 
-    startTime = getTimeByUs();
-    
-    // * Main chain and Second chain of deque
-    std::deque<long>dequeMainChain;
-    std::deque<long>dequeSecondChain;
-
-    // * Dividing into pairs and Sort the pairs in deque
-    for (size_t i = 0; i < deque.size(); i+= 2){
-        // * deque
-        if (deque[i] > deque[i + 1]){
-            std::swap(deque[i], deque[i + 1]);
-        }
-    }
-    
-    // * Forming two chains in deque
-    for (size_t i = 0; i < deque.size(); i+= 2){
-        // * deque
-        dequeMainChain.push_back(deque[i + 1]);
-        dequeSecondChain.push_back(deque[i]);
-    }
-
-    // * check if we has a odd deque (push the last element that we remove it in the begin in second chain of deque)
-    if (!isEven){
-        // * deque
-        dequeSecondChain.push_back(lastElement);
-    }
+    // // * check if we has a odd vector (push the last element that we remove it in the begin in second chain of vector)
+    // if (!isEven){
+    //     // * vector
+    //     vectorSecondChain.push_back(lastElement);
+    // }
     
 
-    // * Sort the main chain of deque
-    std::sort(dequeMainChain.begin(), dequeMainChain.end());
+    // // * Sort the main chain of vector
+    // std::sort(vectorMainChain.begin(), vectorMainChain.end());
 
-    // * Merge chains of deque (Add the small numbers to main chain: every single one we need to add him to correct place)
-    std::deque<long>::iterator itDequeInsert;
-    for (size_t i = 0; i < dequeSecondChain.size(); i++){
-        // * deque
-        itDequeInsert = std::lower_bound(dequeMainChain.begin(), dequeMainChain.end(), dequeSecondChain[i]);
-        dequeMainChain.insert(itDequeInsert, dequeSecondChain[i]);
-    }
+    // // * Merge chains of vector (Add the small numbers to main chain: every single one we need to add him to correct place)
+    // std::vector<long>::iterator itVectorInsert;
+    // for (size_t i = 0; i < vectorSecondChain.size(); i++){
+    //     // * vector
+    //     itVectorInsert = std::lower_bound(vectorMainChain.begin(), vectorMainChain.end(), vectorSecondChain[i]);
+    //     vectorMainChain.insert(itVectorInsert, vectorSecondChain[i]);
+    // }
     
-    // * get time by microseconds after sorting 
-    endTime = getTimeByUs();
+    // // * get time by microseconds after sorting 
+    // double endTime = getTimeByUs();
 
-    // * print time of excution by microseconds
-    std::cout << std::fixed << std::setprecision(5)
-        << "Time to process a range of "
-        << (isEven ? deque.size() : deque.size() + 1) << " elements with std::[deque] : "
-        << endTime - startTime << "µs" << std::endl;
+    // // * print all element after sorting
+    // std::vector<long>::iterator vectorItAfterSort = vectorMainChain.begin();
+    
+    // std::cout << "After: ";
+    
+    // for(; vectorItAfterSort != vectorMainChain.end(); ++vectorItAfterSort){
+    //     std::cout << *vectorItAfterSort << " ";
+    // }
+
+    // std::cout << std::endl;
+
+    // // * print time of excution by microseconds
+    // std::cout << std::fixed << std::setprecision(5)
+    //     << "Time to process a range of "
+    //     << (isEven ? vector.size() : vector.size() + 1) << " elements with std::[vector] : "
+    //     << endTime - startTime << "µs" << std::endl;
+
+
+    // // ! Sort the Deque
+    // // * get time by microseconds befor sorting 
+    // startTime = getTimeByUs();
+    
+    // // * Main chain and Second chain of deque
+    // std::deque<long>dequeMainChain;
+    // std::deque<long>dequeSecondChain;
+
+    // // * Dividing into pairs and Sort the pairs in deque
+    // for (size_t i = 0; i < deque.size(); i+= 2){
+    //     // * deque
+    //     if (deque[i] > deque[i + 1]){
+    //         std::swap(deque[i], deque[i + 1]);
+    //     }
+    // }
+    
+    // // * Forming two chains in deque
+    // for (size_t i = 0; i < deque.size(); i+= 2){
+    //     // * deque
+    //     dequeMainChain.push_back(deque[i + 1]);
+    //     dequeSecondChain.push_back(deque[i]);
+    // }
+
+    // // * check if we has a odd deque (push the last element that we remove it in the begin in second chain of deque)
+    // if (!isEven){
+    //     // * deque
+    //     dequeSecondChain.push_back(lastElement);
+    // }
+    
+
+    // // * Sort the main chain of deque
+    // std::sort(dequeMainChain.begin(), dequeMainChain.end());
+
+    // // * Merge chains of deque (Add the small numbers to main chain: every single one we need to add him to correct place)
+    // std::deque<long>::iterator itDequeInsert;
+    // for (size_t i = 0; i < dequeSecondChain.size(); i++){
+    //     // * deque
+    //     itDequeInsert = std::lower_bound(dequeMainChain.begin(), dequeMainChain.end(), dequeSecondChain[i]);
+    //     dequeMainChain.insert(itDequeInsert, dequeSecondChain[i]);
+    // }
+    
+    // // * get time by microseconds after sorting 
+    // endTime = getTimeByUs();
+
+    // // * print time of excution by microseconds
+    // std::cout << std::fixed << std::setprecision(5)
+    //     << "Time to process a range of "
+    //     << (isEven ? deque.size() : deque.size() + 1) << " elements with std::[deque] : "
+    //     << endTime - startTime << "µs" << std::endl;
 }
