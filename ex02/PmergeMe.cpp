f1bdcc8c46e0c4d9e8b8ed1d59efec4cf9650972
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-11-23 11:15:55 +0100

    alot of cases damn

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -33,14 +33,17 @@ bool comparison(long a, long b){
 
 
 static bool getJacobsthalNumber(std::vector<std::pair<std::string, std::vector<long>>>&pendChain,
-    std::vector<long>&jacobsthalNumberVector, int sizeOfPairs){
+    std::vector<long>&jacobsthalNumberVector){
     // * clear the old data from jacobsthalNumberVector befor add the new numbers
     jacobsthalNumberVector.clear();
 
     // * get the get Jacobsthal Number
-    for (size_t i = 0; i < pendChain.size() / sizeOfPairs; i++){
+    for (size_t i = 0; i < pendChain.size(); i++){
+        // * we add 1 to skip letter b
+        double numOfChainWithoutBLetter = std::strtod(pendChain[i].first.c_str() + 1, NULL);
+
         for (size_t j = 0; j < 65; j++){
-            if (i + 2 == jacobNums[j]){
+            if (numOfChainWithoutBLetter == jacobNums[j]){
                 // * store the Jacobsthal Number in vector
                 jacobsthalNumberVector.push_back(jacobNums[j - 1]); // * store the prev jacobsthal number first
                 jacobsthalNumberVector.push_back(jacobNums[j]); // * store the jacobsthal number second
@@ -56,13 +59,10 @@ static bool getJacobsthalNumber(std::vector<std::pair<std::string, std::vector<l
     return true;
 }
 
-
-
-// static void mergeMainAndPendChains(std::vector<long>& mainChain,
-//     std::vector<long>& pendChain){
-        
-// }
-
+// * function of lower bound to get index of insert
+bool compFunc(std::pair<std::string, std::vector<long> > &pair, long value){
+    return pair.second.back() < value;   // * same as default
+}
 
 
 
@@ -158,37 +158,204 @@ static void divisionIntoPairsAndSorting(std::vector<long>&vector, int sizeOfPair
             }
         }
 
-        // ? insert the pend to main
-        // ! CHECK IF PEND EMPTY
+        // ? Insert the pend to main
+        // * check if pend empty
         if (pendChain.empty()){
             return;
         }
         
-        // * get jacobsthal number
-        // std::vector<long>jacobsthalNumberVector;
-        // if (getJacobsthalNumber(pendChain, jacobsthalNumberVector, sizeOfPairs)){
-        //     // * get number of element that we need to push it to main chain
-        //     size_t numberOfElementThatWePush = jacobsthalNumberVector[1] - jacobsthalNumberVector[0];
-        //     for (size_t i = 0; i < numberOfElementThatWePush; i++){
-        //         // ! GET THE LARGER NUMBER OF PAIR TO DO BINARY SEARCH
-        //         long largeElemenOfPair = pendChain[jacobsthalNumberVector[1] + sizeOfPairs];
-        //         std::cout << "largeElemenOfPair: " << largeElemenOfPair << std::endl;
-        //         for (size_t j = 0; j < sizeOfPairs; j++){
-        //             // ! PUSH THE ELEMENT OF PAIR TO PEND CHAIN
-        //             std::vector<long>::iterator itBound = std::lower_bound(mainChain.begin(), mainChain.end(), largeElemenOfPair);
-        //             std::cout << "*itBound: " << *itBound << std::endl;
-    
-        //             // jacobsthalNumberVector.erase(jacobsthalNumberVector.begin());
-        //             // jacobsthalNumberVector.erase(jacobsthalNumberVector.begin() + 1);
-        //         }
-        //     }
+        // * get jacobsthal number and insert
+        std::vector<long>jacobsthalNumberVector;
+        if (getJacobsthalNumber(pendChain, jacobsthalNumberVector)){
 
-
-        // } else {
-            
-        // }
-
+            while (!pendChain.empty()){
+                if (jacobsthalNumberVector.size() >= 2){
+                    // * get number of element that we need to push it to main chain
+                    size_t numberOfElementThatWePush = jacobsthalNumberVector[1] - jacobsthalNumberVector[0];
+                    
+                    // * create the key of pair from jacobsthal number
+                    std::string key = "b";
         
+                    std::stringstream ss;
+                    ss << jacobsthalNumberVector[1];
+                    key += ss.str();
+        
+                    // * get index of pair by jacobsthal number
+                    size_t j = 0;
+                    for (; j < pendChain.size(); j++){
+                        if (pendChain[j].first == key){
+                            break;
+                        }
+                    }
+    
+                    // * check if we have a pair from as (like if we have b3 we need to check if we have a3 to limit the range of searching)
+                    key[0] = 'a';
+        
+                    // * get end of range that we need to search in it
+                    size_t endOfRange = -1;
+                    for (size_t k = 0; k < mainChain.size(); k++){
+                        if (mainChain[k].first == key){
+                            endOfRange = k;
+                            break;
+                        }
+                        
+                    }
+        
+                    // * clear key
+                    key.clear();
+    
+                    // * now insert pair of pend to main chain
+                    for (size_t count = 0; (count < numberOfElementThatWePush) && !pendChain.empty(); count++){
+                        // * get the larger number of pair to do binary search
+                        long largeElemenOfPair = pendChain[j].second.back();
+    
+                        // * push the element of pair to main chain using lower bound
+                        std::vector<std::pair<std::string, std::vector<long>>>::iterator itLowerBound;
+            
+                        // * check range
+                        if (endOfRange == -1){
+                            itLowerBound = std::lower_bound(mainChain.begin(), mainChain.end(), largeElemenOfPair, compFunc);
+                        } else {
+                            itLowerBound = std::lower_bound(mainChain.begin(), mainChain.begin() + endOfRange, largeElemenOfPair, compFunc);
+                        }
+            
+                        // * insert pair from pend to main chain
+                        mainChain.insert(itLowerBound, pendChain[j]);
+                        // * remove him from pend
+                        pendChain.erase(pendChain.begin() + j);
+                        // * remove jacobsthal numbers (current and prev) that we use
+                        if (jacobsthalNumberVector.size() >= 2){
+                            jacobsthalNumberVector.erase(jacobsthalNumberVector.begin(), jacobsthalNumberVector.begin() + 1);
+                        }                        
+                        // * decrement the var j to go to next pair because we are push element by revers order
+                        if (j > 0){
+                            j--;
+                        }
+                    }
+                } else {
+                    // * check if we are use all jacobsthal numbers but we still have an pair in pend chain
+                    // * then we need to push all rest pairs by revers like from end to start
+                    for (size_t count = pendChain.size() - 1; count >= 0; count--){
+                        // * get the larger number of pair to do binary search
+                        long largeElemenOfPair = pendChain[count].second.back();
+
+                        // * get key of pair to search the the bound of him
+                        std::string key = pendChain[count].first;
+
+                        // * change the first character from b to a (like from b7 to a7)
+                        key[0] = 'a';
+
+                        // * get end of range that we need to search in it
+                        size_t endOfRange = -1;
+                        for (size_t k = 0; k < mainChain.size(); k++){
+                            if (mainChain[k].first == key){
+                                endOfRange = k;
+                                break;
+                            }
+                            
+                        }
+            
+                        // * clear key
+                        key.clear();
+
+                        // * push the element of pair to main chain using lower bound
+                        std::vector<std::pair<std::string, std::vector<long>>>::iterator itLowerBound;
+            
+                        // * check range
+                        if (endOfRange == -1){
+                            itLowerBound = std::lower_bound(mainChain.begin(), mainChain.end(), largeElemenOfPair, compFunc);
+                        } else {
+                            itLowerBound = std::lower_bound(mainChain.begin(), mainChain.begin() + endOfRange, largeElemenOfPair, compFunc);
+                        }
+
+                        // * insert pair from pend to main chain
+                        mainChain.insert(itLowerBound, pendChain[count]);
+                        // * remove him from pend
+                        pendChain.erase(pendChain.begin() + count);
+                    }
+                    
+                }
+                
+            }
+
+        } else {
+            // * check if there no jacobsthal numbers but we still have an pair in pend chain
+            // * then we need to push all pairs by revers like from end to start
+            for (size_t count = pendChain.size() - 1; count >= 0; count--){
+                // * get the larger number of pair to do binary search
+                long largeElemenOfPair = pendChain[count].second.back();
+
+                // * get key of pair to search the the bound of him
+                std::string key = pendChain[count].first;
+
+                // * change the first character from b to a (like from b7 to a7)
+                key[0] = 'a';
+
+                // * get end of range that we need to search in it
+                size_t endOfRange = -1;
+                for (size_t k = 0; k < mainChain.size(); k++){
+                    if (mainChain[k].first == key){
+                        endOfRange = k;
+                        break;
+                    }
+                    
+                }
+    
+                // * clear key
+                key.clear();
+
+                // * push the element of pair to main chain using lower bound
+                std::vector<std::pair<std::string, std::vector<long>>>::iterator itLowerBound;
+    
+                // * check range
+                if (endOfRange == -1){
+                    itLowerBound = std::lower_bound(mainChain.begin(), mainChain.end(), largeElemenOfPair, compFunc);
+                } else {
+                    itLowerBound = std::lower_bound(mainChain.begin(), mainChain.begin() + endOfRange, largeElemenOfPair, compFunc);
+                }
+
+                // * insert pair from pend to main chain
+                mainChain.insert(itLowerBound, pendChain[count]);
+                // * remove him from pend
+                pendChain.erase(pendChain.begin() + count);
+            }
+            
+        }
+
+        // * get rest element of vector that not belong to any pair
+        // * create a vector to store the rest element
+        std::vector<long> theRestOfVector;
+        if (mainChain.size() * sizeOfPairs < vector.size()){
+            size_t theRest = vector.size() - mainChain.size() * sizeOfPairs;
+            for (size_t i = vector.size() - 1; theRest > 0; i--){
+                theRestOfVector.insert(theRestOfVector.begin(), vector[i]);
+                theRest--;
+            }
+        }
+
+        // * clear the original vector and push to him main chain and the rest
+        // * clear the vector
+        vector.clear();
+
+        // * push the element to him
+        for (size_t i = 0; i < mainChain.size(); i++){
+            for (size_t j = 0; j < mainChain[i].second.size(); j++){
+                vector.push_back(mainChain[i].second[j]);
+            }
+        }
+
+        // * add the rest if we have it
+        if (!theRestOfVector.empty()){
+            for (size_t i = 0; i < theRestOfVector.size(); i++){
+                vector.push_back(theRestOfVector[i]);
+            }
+            
+        }
+        
+        
+        // * clear the main chain
+        mainChain.clear();
+
         std::cout << "================================" << std::endl;
         
         // * print the vector of main chain
@@ -211,8 +378,23 @@ static void divisionIntoPairsAndSorting(std::vector<long>&vector, int sizeOfPair
             std::cout << std::endl;
         }
 
+        // * print the rest vector 
+        std::cout << "rest: " << std::endl;
+        for (size_t i = 0; i < theRestOfVector.size(); i++){
+            std::cout << theRestOfVector[i] << " ";
+        }
+        std::cout << std::endl;
 
-        exit(0);
+        
+        // * print the original vector 
+        std::cout << "the original vector: " << std::endl;
+        for (size_t i = 0; i < vector.size(); i++){
+            std::cout << vector[i] << " ";
+        }
+        std::cout << std::endl;
+
+
+        // exit(0);
         return;
 }
 
